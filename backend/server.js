7f8ed0e82ce426743e5ce57922fb3901cc7dbcc4
Ajayk66 <ajaymmss11@gmail.com>
2026-03-13 17:45:24 +0530

    Configure for Render deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { serve } from 'inngest/express';
 import { inngest } from './inngest/client.js';
 import { processTicketAiAnalysis, sendStatusUpdateEmail, sendTicketCreatedEmail } from './inngest/functions.js';
@@ -24,6 +26,22 @@ app.use(
     functions: [processTicketAiAnalysis, sendTicketCreatedEmail, sendStatusUpdateEmail],
   })
 );
+
+// Serve frontend in production
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
